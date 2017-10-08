98e00249595f67a001446ea30ec434dd100f6c37
MAC <surprisepeas@gmail.com>
2017-10-08 23:50:38 +0800

    * swoole 消息队列 雏形

--- console/config/main.php
+++ console/config/main.php
@@ -20,6 +20,13 @@ return [
                 ],
             ],
         ],
+        'redis' => [
+            'class' => 'yii\redis\Connection',
+            'hostname' => '127.0.0.1',
+            'port' => 6379,
+            'database' => 1,
+            'password' => 'xiaolin'
+        ],
     ],
     'params' => $params,
 ];
--- console/controllers/LogController.php
+++ console/controllers/LogController.php
@@ -23,15 +23,18 @@ class LogController extends Controller
 
     protected $processHandleMaxNumber = 1000;
 
+    public static $instances;
+
     public function actionHandle()
     {
+        ini_set('default_socket_timeout', -1);
         if (!extension_loaded('swoole')) {
             print_r('The swoole extension is not installed');
             return;
         }
 
         // install signal handler for dead kids
-        pcntl_signal(SIGCHLD, [$this, "signalHandler"]);
+        pcntl_signal(SIGCHLD, [&$this, "signalHandler"]);
         set_time_limit(0);
         $redis = Yii::$app->redis;
         while (true) {
@@ -39,12 +42,131 @@ class LogController extends Controller
             if ($this->process < $this->maxProcesses) {
                 // 无任务时,阻塞等待
                 $data = $redis->brpop($this->queueKey, 3);
-                if (!$data) {
+                if (empty($data)) {
                     continue;
+                }
+                if ($data[0] != $this->queueKey) {
+                    // 消息队列KEY值不匹配
+                    continue;
+                }
+                if (isset($data[1])) {
+                    $process = new \swoole_process([$this, 'task']);
+                    $process->write($this->rewrite($data[1]));
+                    $pid = $process->start();
+                    if ($pid === false) {
+                        $redis->lpush($this->queueKey, $data[1]);
+                    } else {
+                        $this->process++;
+                    }
+                }
+            } else {
+                dump('结束');
+                if (is_int($this->waittime) && $this->waittime > 0) {
+                    sleep($this->waittime);
                 }
             }
         }
     }
 
+    /**
+     * @desc   主进程中操作数据
+     * @tip    主进程中不能实例化DB类，因为Mysql连接会中断
+     *         暂时原因不明，可能是会被子进程释放掉
+     * @author limx
+     * @param $data 消息队列中的数据
+     * @return mixed 返回给子进程的数据
+     */
+    protected function rewrite($data)
+    {
+        return $data;
+    }
+
+    /**
+     * @desc   信号处理方法 回收已经dead的子进程
+     * @author limx
+     * @param $signo
+     */
+    private function signalHandler($signo)
+    {
+        switch ($signo) {
+            case SIGCHLD:
+                while (\swoole_process::wait(false)) {
+                    $this->process--;
+                }
+
+            default:
+                break;
+        }
+    }
+
+    /**
+     * @desc   子进程
+     * @author limx
+     * @param swoole_process $worker
+     */
+    public function task(\swoole_process $worker)
+    {
+        \swoole_event_add($worker->pipe, function ($pipe) use ($worker) {
+            // 从主进程中读取到的数据
+            $recv = $worker->read();
+            $this->again($recv);
+            $worker->exit(0);
+            \swoole_event_del($pipe);
+        });
+    }
+
+    /**
+     * @desc   消息队列子进程逻辑
+     * @author limx
+     * @return mixed
+     */
+    public function again($recv)
+    {
+        $this->handle($recv);
+        $redis = Yii::$app->redis;
+        $number = 0;
+        while (true) {
+            if (isset($this->processHandleMaxNumber) && $this->processHandleMaxNumber < (++$number)) {
+                // 当子进程处理次数高于一个临界值后，释放进程
+                break;
+            }
+            // 无任务时,阻塞等待
+            $data = $redis->brpop($this->queueKey, 3);
+            if (empty($data)) {
+                break;
+            }
+            if ($data[0] != $this->queueKey) {
+                // 消息队列KEY值不匹配
+                continue;
+            }
+            if (isset($data[1])) {
+                $this->handle($data[1]);
+            }
+        }
+    }
+
+    public function handle($data)
+    {
+        $request = json_decode($data, true);
+        dump($request);
+    }
+
+    /**
+     * 测试数据
+     */
+    public function actionTest()
+    {
+        $redis = Yii::$app->redis;
+        $data = [
+            'name' => 'xiaolin',
+            'email' => '462441355@qq.com',
+            'password' => uniqid(),
+        ];
+        $redis->lpush($this->queueKey, json_encode($data));
+//        while (true){
+//            $data = $redis->brpop($this->queueKey, 3);
+//            dump($data);
+//        }
+    }
 
 }
