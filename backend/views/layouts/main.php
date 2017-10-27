edf6c9fba6f8006389d57dd4fb00a3745d5664a5
xiaolin <462441355@qq.com>
2017-10-27 23:17:10 +0800

    * 后台基本搭建

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -19,19 +19,40 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
+    <title><?= Html::encode($this->params['meta_title'] ?? '') ?></title>
+    <meta name="Keywords" content="<?= Html::encode($this->params['meta_keyword'] ?? '') ?>"/>
+    <meta name="Description" content="<?= Html::encode($this->params['meta_description'] ?? '') ?>"/>
+    <link rel="stylesheet" href="/www/css/app.css">
     <?php $this->head() ?>
 </head>
 <body>
 <?php $this->beginBody() ?>
-<div id="top">
-</div>
-<div id="container">
-    <?= $content ?>
-</div>
+<div id="app">
+    <?php if (empty(Yii::$app->user->isGuest)) { ?>
+        <div class="header">
+            <?= $this->render('../partials/header'); ?>
+        </div>
+    <?php } ?>
+    <div class="content">
+        <?php if (empty(Yii::$app->user->isGuest)) { ?>
+            <div class="sidenav">
+                <?= $this->render('../partials/sidenav'); ?>
+            </div>
+        <?php } ?>
 
-<footer class="footer">
-</footer>
+        <div class="main-content">
+            <div class="shell-content">
+                <?= $content ?>
+            </div>
+        </div>
+    </div>
+    <?php if (empty(Yii::$app->user->isGuest)) { ?>
+        <div class="footer">
+            <?= $this->render('../partials/footer'); ?>
+        </div>
+    <?php } ?>
+</div>
+<link rel="stylesheet" href="/www/js/app.js">
 
 <?php $this->endBody() ?>
 </body>
---
+++ backend/views/partials/header.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div class="logo-box">
+                <a href="/">小林后台信息管理系统</a>
+            </div>
+
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle">
+                        Hi，小林
+                    </a>
+                </li>
+                <li>
+                    <a href="">退出</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
