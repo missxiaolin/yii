b726ca1bda3bbb6fb8d030c01ed1450abb09c2e2
xiaolin <462441355@qq.com>
2017-10-30 09:45:49 +0800

    * assets 初始化

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,4 +3,5 @@ Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
+Yii::setAlias('@assets', dirname(dirname(__DIR__)) . '/assets');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
