a4f89789c79e97a90296efef96996a700e7f5bb1
xiaolin <462441355@qq.com>
2017-11-26 11:44:33 +0800

    * 权限管理rbac配置

--- backend/config/main.php
+++ backend/config/main.php
@@ -13,6 +13,18 @@ return [
     'bootstrap' => ['log'],
     'modules' => [],
     'components' => [
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            // auth_item (role permission)
+            // auth_item_child (role->permission)
+            // auth_assignment (user->role)
+            // auth_rule (rule)
+            'itemTable' => '{{%auth_item}}',
+            'itemChildTable' => '{{%auth_item_child}}',
+            'assignmentTable' => '{{%auth_assignment}}',
+            'ruleTable' => '{{%auth_rule}}',
+            'defaultRoles' => ['default'],
+        ],
         'request' => [
             'csrfParam' => '_csrf-backend',
             'enableCookieValidation' => false,
