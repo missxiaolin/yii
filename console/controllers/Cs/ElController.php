26cc9ef812d23a84fa96e3a75975287710b0ef82
xiaolin <462441355@qq.com>
2017-12-02 11:46:05 +0800

    * es查询

--- console/controllers/Cs/ElController.php
+++ console/controllers/Cs/ElController.php
@@ -11,28 +11,56 @@ class ElController extends Controller
 {
     public function actionIndex()
     {
-        try{
+        try {
             $shop_model = ShopsModel::find()->where(['shop_id' => 263])->one();
-
+//            dump(ShopsElModel::type());
             $el_shop_model = new ShopsElModel();
             $el_shop_model->primaryKey = $shop_model->shop_id;
             $el_shop_model->shop_name = $shop_model->shop_name;
             $el_shop_model->longitude = $shop_model->longitude;
             $el_shop_model->latitude = $shop_model->latitude;
             $el_shop_model->save();
-        }catch (Exception $e){
+        } catch (Exception $e) {
             dump($e->getMessage());
         }
     }
 
-    public function actionGetShop()
+    public function actionGetShop($keyword)
     {
-        try{
-//            $el_shop_models = ShopsElModel::find()->where(['shop_name'=>'周渝食惦'])->all();
+        try {
 //            $el_shop_models = ShopsElModel::find()->all();
-            $el_shop_models = ShopsElModel::find()->where(['shop_nam'=>'周渝'])->search();
+//            $el_shop_models = ShopsElModel::find()
+//                ->query([
+//                    'multi_match' => [
+//                        'query' => '周',
+//                        'fields' => ['shop_id', 'shop_name', 'longitude', 'latitude'],
+//                    ],
+//                ])
+//                ->highlight([
+//                    'pre_tags'=>['<em>'],
+//                    'post_tags'=>['</em>'],
+//                    'fields'=>[
+//                        'shop_name'=> new \stdClass()
+//                    ]
+//                ])->all();
+            $el_shop_models = ShopsElModel::find()
+                ->query([
+                    'bool' => [
+                        'must' => [
+                            ['match' => ['shop_name' => $keyword]],
+                        ],
+                    ],
+                ])
+                ->highlight([
+                    'pre_tags' => ['<em>'],
+                    'post_tags' => ['</em>'],
+                    'fields' => [
+                        'shop_name' => new \stdClass(),
+                    ],
+                ])
+                ->all();
             dump($el_shop_models);
-        }catch (Exception $e){
+        } catch (Exception $e) {
             dump($e->getMessage());
         }
 
