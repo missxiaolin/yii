6b5a72dddb0d29027ab58b0a7fbbebff37561e04
xiaolin <462441355@qq.com>
2017-12-03 14:50:24 +0800

    * kafka测试

---
+++ console/controllers/Cs/KafkaController.php
@@ -0,0 +1,36 @@
+<?php
+namespace console\controllers\Cs;
+
+use common\components\Kafka\Kafka;
+use yii\console\Controller;
+use Exception;
+use Yii;
+
+class KafkaController extends Controller
+{
+    /**
+     * 添加
+     */
+    public function actionIndex()
+    {
+        $kafka = new Kafka();
+        $kafka->send(1);
+    }
+
+    /**
+     * 消费
+     */
+    public function actionConsumer()
+    {
+        $kafka = new Kafka();
+        $kafka->consumer($this,'handle');
+    }
+
+    /**
+     * @param $message
+     */
+    public function handle($message)
+    {
+        Yii::info($message);
+    }
+}
