42eb0e7eb22940af9fde093cafb5c84bc536b71d
xiaolin <462441355@qq.com>
2018-01-28 16:49:39 +0800

    * el 测试增加add  和  term测试

--- console/controllers/search/ArticleController.php
+++ console/controllers/search/ArticleController.php
@@ -1,6 +1,7 @@
 <?php
 namespace console\controllers\search;
 
+use Carbon\Carbon;
 use console\controllers\utils\Elasticsearch\Client;
 use console\controllers\utils\Elasticsearch\ES;
 use yii\console\Controller;
@@ -19,6 +20,8 @@ class ArticleController extends Controller
         echo 'mapping       创建mapping' . PHP_EOL;
         echo 'getMapping    读取mapping' . PHP_EOL;
         echo 'del           删除整个索引' . PHP_EOL;
+        echo 'add           增加数据' . PHP_EOL;
+        echo 'term          根据term进行搜索' . PHP_EOL;
     }
 
     /**
@@ -59,7 +62,7 @@ class ArticleController extends Controller
                     'articleID' => ['type' => 'string'],
                     'userId' => ['type' => 'integer'],
                     'hidden' => ['type' => 'boolean'],
-                    'postDate' => ['type' => 'date']
+                    'postDate' => ['type' => 'date'],
                 ],
             ],
         ];
@@ -94,4 +97,94 @@ class ArticleController extends Controller
             dump($res);
         }
     }
+
+    /**
+     * 删除整个索引
+     */
+    public function actionDel()
+    {
+        $client = Client::getInstance();
+        $indices = $client->indices();
+
+        $params = [
+            'index' => $this->index,
+        ];
+        try {
+            $res = $indices->delete($params);
+            if ($res['acknowledged']) {
+                dump('删除索引成功');
+            }
+        } catch (\Exception $ex) {
+            $res = json_decode($ex->getMessage(), true);
+            dump($res);
+        }
+    }
+
+    /**
+     * 创建数据
+     */
+    public function actionAdd()
+    {
+        $client = Client::getInstance();
+        for ($i = 0; $i < 5; $i++) {
+            $params = [
+                'index' => $this->index,
+                'type' => $this->type,
+                'id' => $i,
+                'body' => [
+                    'articleID' => uniqid(),
+                    'hidden' => $i % 2 ? true : false,
+                    'postDate' => time(),
+                    'userId' => $i,
+                ],
+            ];
+            try {
+                $res = $client->index($params);
+                dump($res);
+                if ($res['created']) {
+                    echo '用户DOC创建成功' . PHP_EOL;
+                } else {
+                    echo '用户DOC创建失败（可能已存在））' . PHP_EOL;
+                }
+            } catch (\Exception $ex) {
+                dump($ex->getMessage());
+            }
+        }
+    }
+
+    /**
+     * 根绝userId、hidden搜索
+     */
+    public function actionTerm()
+    {
+        $client = Client::getInstance();
+        $res = [];
+        try {
+            $params = [
+                'index' => $this->index,
+                'type' => $this->type,
+                'body' => [
+                    'query' => [
+                        'constant_score' => [
+                            'filter' => [
+                                'term' => [
+//                                    'userId' => 1,
+                                    'hidden' => true
+                                ],
+                            ],
+                        ],
+                    ],
+                    'from' => 0,
+                    'size' => 5,
+                ],
+            ];
+            $res = $client->search($params);
+        } catch (\Exception $e) {
+            dump($e->getMessage());
+        }
+        dd($res);
+
+    }
+
+
 }
