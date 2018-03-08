09fc112ac9f12636d8852b0c75ef4a1effcd3458
xiaolin <462441355@qq.com>
2018-03-08 15:42:56 +0800

    * 测试workman

--- console/controllers/Cs/work/CsController.php
+++ console/controllers/Cs/work/CsController.php
@@ -7,9 +7,26 @@ use Yii;
 
 class CsController extends Controller
 {
-    public function actionTest()
+    public function actionTest($action)
     {
-        $ws_worker = new Worker("websocket://0.0.0.0:2346");
-        dd($ws_worker);
+        global $argv;
+        $argv[0]='测试脚本';
+        $argv[1]=$action;
+        $argv[2]='-d';
+        // 创建一个Worker监听2345端口，使用http协议通讯
+        $http_worker = new Worker("http://127.0.0.1:2345");
+
+        // 启动4个进程对外提供服务
+        $http_worker->count = 4;
+
+        // 接收到浏览器发送的数据时回复hello world给浏览器
+        $http_worker->onMessage = function($connection, $data)
+        {
+            // 向浏览器发送hello world
+            $connection->send('hello world');
+        };
+
+        // 运行worker
+        Worker::runAll();
     }
 }
---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2018/3/8
+ * Time: 下午3:28
+ */
+
+print_r($argv);
