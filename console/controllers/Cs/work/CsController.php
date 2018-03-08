ce6bb1695b4ba096951263492f4b11f7dba52f60
xiaolin <462441355@qq.com>
2018-03-08 18:21:56 +0800

    * 格式化代码

--- console/controllers/Cs/work/CsController.php
+++ console/controllers/Cs/work/CsController.php
@@ -7,12 +7,16 @@ use Yii;
 
 class CsController extends Controller
 {
+    /**
+     * workman
+     * @param $action
+     */
     public function actionTest($action)
     {
         global $argv;
-        $argv[0]='测试脚本';
-        $argv[1]=$action;
-        $argv[2]='-d';
+        $argv[0] = '测试脚本';
+        $argv[1] = $action;
+        $argv[2] = '-d';
         // 创建一个Worker监听2345端口，使用http协议通讯
         $http_worker = new Worker("http://127.0.0.1:2345");
 
@@ -20,8 +24,7 @@ class CsController extends Controller
         $http_worker->count = 4;
 
         // 接收到浏览器发送的数据时回复hello world给浏览器
-        $http_worker->onMessage = function($connection, $data)
-        {
+        $http_worker->onMessage = function ($connection, $data) {
             // 向浏览器发送hello world
             $connection->send('hello world');
         };
