30ab134e05b1a40d01cddbcf75e3fa90a3c5ce9a
xiaolin <462441355@qq.com>
2018-06-12 10:05:41 +0800

    * 修改配置文件

--- console/config/main.php
+++ console/config/main.php
@@ -49,7 +49,7 @@ return [
             'hostname' => '127.0.0.1',
             'port' => 6379,
             'database' => 1,
-            'password' => 'xiaolin',
+            'password' => null,
         ],
         'resque' => [
             'class' => 'common\components\Yii2Resque',
