f1b8ff5c424180df173c17527479292c15f246e2
xiaolin <462441355@qq.com>
2018-10-08 10:57:37 +0800

    * 新增错误类

---
+++ common/components/Range/RangeInvalidException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2018/10/8
+ * Time: 上午10:56
+ */
+
+namespace common\components\Range;
+
+use Exception;
+
+/**
+ * Class RangeInvalidException
+ * @package common\components\Range
+ */
+class RangeInvalidException extends Exception
+{
+
+}
